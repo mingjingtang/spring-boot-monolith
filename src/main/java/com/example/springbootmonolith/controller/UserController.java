08eb4bbd0c700c0e485dd7b6f2bad863a27c76d3
mingjingtang <tmjcsh@gmail.com>
2020-08-02 22:42:02 -0400

    create user role models

--- src/main/java/com/example/springbootmonolith/controller/UserController.java
+++ src/main/java/com/example/springbootmonolith/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.springbootmonolith.controller;
 import com.example.springbootmonolith.models.User;
 import com.example.springbootmonolith.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +11,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-//    @GetMapping("/hello")
-//    public String helloWorld(){
-//        return "Hello World!!";
-//    }
 
     @GetMapping("/list")
     public Iterable<User> listUsers(){
---
+++ src/main/java/com/example/springbootmonolith/models/UserRole.java
@@ -0,0 +1,34 @@
+package com.example.springbootmonolith.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class UserRole {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String name;
+
+    public UserRole(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
